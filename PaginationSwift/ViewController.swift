d5d8bc6cefe1061489e42c45828881b5925f3369
Daniyal Yousuf <daniyal.yousuf@koderlabs.com>
2018-03-21 14:00:11 +0500

    Initial Commit

---
+++ PaginationSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaginationSwift
+//
+//  Created by Daniyal Yousuf on 3/21/18.
+//  Copyright © 2018 Daniyal Yousuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
